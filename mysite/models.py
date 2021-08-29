1e3c4a16ec291997824b49d960e7f99925f4e1b4
Shen_SJ <johnson840205@gmail.com>
2021-08-29 23:28:32 +0800

    Add tags_cloud

--- mysite/models.py
+++ mysite/models.py
@@ -7,6 +7,9 @@ from ckeditor.fields import RichTextField
 class Tags(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
 
--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from mysite import models
+from collections import OrderedDict
 
 
 # Create your views here.
@@ -10,9 +11,48 @@ def recent_posts(number=5):
     return recent_articles
 
 
+def tags_cloud():
+    # determine all tags frequency
+    tag_freq = {}
+    for tag in models.Tags.objects.all():
+        tag_freq[tag.name] = len(models.Articles.objects.filter(tags=tag))
+    most_freq = max(tag_freq.values())
+    interval = (most_freq - 1) / 6
+
+    # classified depends on the tag frequency
+    tags_class = OrderedDict()
+    for tag in models.Tags.objects.all().order_by('name'):
+        tag_class = None
+        if most_freq == len(models.Articles.objects.filter(tags=tag)):
+            tag_class = 0
+        elif (most_freq > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= most_freq-interval):
+            tag_class = 1
+        elif (most_freq-interval > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= most_freq-interval*2):
+            tag_class = 2
+        elif (most_freq-interval*2 > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= most_freq-interval*3):
+            tag_class = 3
+        elif (most_freq-interval*3 > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= most_freq-interval*4):
+            tag_class = 4
+        elif (most_freq-interval*4 > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= most_freq-interval*5):
+            tag_class = 5
+        elif (most_freq-interval*5 > len(models.Articles.objects.filter(tags=tag))) and \
+             (len(models.Articles.objects.filter(tags=tag)) >= 1):
+            tag_class = 6
+        elif len(models.Articles.objects.filter(tags=tag)) == 0:
+            continue
+        tags_class[tag.name] = tag_class
+    return tags_class
+
+
 def index(request):
     # show the recent posts in sidebar
     recent_articles = recent_posts()
+    tags_classified = tags_cloud()
 
     # list all of the articles in the body
     articles = models.Articles.objects.filter(visible=True).order_by('-pub_date')
@@ -31,6 +71,7 @@ def index(request):
 def article_page(request, slug):
     # show the recent posts in sidebar
     recent_articles = recent_posts()
+    tags_classified = tags_cloud()
     try:
         article = models.Articles.objects.get(slug=slug)
         article = {
