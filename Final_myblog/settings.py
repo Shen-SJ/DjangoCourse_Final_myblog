54af7d5b2e278ff24a4c889c96c5b235aa4665c9
Shen_HJ <johnson840205@gmail.com>
2022-05-07 11:48:45 +0800

    Build Dark/Light theme function, web safe setting change.
    1. build dark/flat theme in one css (finish dark theme).
    2. add theme toggle icon and js file on navbar.

--- Final_myblog/settings.py
+++ Final_myblog/settings.py
@@ -25,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&&-b+1rz@qylqitcj7if59yr8-)cd$ym%*671lu-5-@=85-x%g'
+SECRET_KEY = key_data["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
-# DEBUG = True
+# DEBUG = False
 #
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
